97c4f21b3f1e7b18eaf5fb6dc6541181035eaa52
Bas Pijls <bpijls@gmail.com>
2016-12-07 11:27:07 +0100

    Changes WeatherChecker example to accommodate for BuienRadar changes

--- examples/WeatherChecker/WeatherChecker.ino
+++ examples/WeatherChecker/WeatherChecker.ino
@@ -9,23 +9,21 @@ void setup() {
   delay(10);
 
   hotspot.begin("HomeNetwork", "HomePassword");
+
 }
 
 void loop() {
   HTTPClient http;
-  http.begin("http://gadgets.buienradar.nl/data/raintext?lat=52.3409792&lon=4.9077414");
-  uint16_t httpCode = http.GET();
-  String response;
-
-  if (httpCode == 200) {
-    response = http.getString();
+  http.begin("http://gadgets.buienradar.nl/data/raintext/?lat=52.34&lon=4.91");
+  {
+    uint16_t httpCode = http.GET();
+    String response = http.getString();
     Serial.println(response);
-  }
-  else
-     ESP.reset();
 
+    if (httpCode != 200) ESP.reset();
+  }
   http.end();
 
-  
-  delay(60*1000*5);  
+  // Wait for 5 minutes
+  delay(60 * 1000 * 5);
 }
